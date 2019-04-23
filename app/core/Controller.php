ecada07c03638efa1b2c9fd8fa9ccfbc234a492a
Michael <mkleyster@mail.ua>
2019-04-23 19:37:19 +0300

    add view and model properties

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -2,8 +2,14 @@
 
 namespace core;
 
-
 abstract class Controller {
-    
+
+    protected $model;
+    protected $view;
+
+    public function __construct() {
+	$this->view = new View();
+    }
+
     abstract public function action_index();
 }
