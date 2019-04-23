a4c9c17b1fa25d2128f2670e61a1ad0637a2654f
Michael <mkleyster@mail.ua>
2019-04-23 19:35:29 +0300

    404 error

--- app/controllers/ControllerMain.php
+++ app/controllers/ControllerMain.php
@@ -4,9 +4,16 @@ namespace controllers;
 
 use core\Controller;
 
-class ControllerMain extends Controller{
+class ControllerMain extends Controller {
+
     public function action_index() {
-	echo 'Main controller action index';//TODO delete 
+	echo 'Main controller action index'; //TODO delete 
 	//TODO action
     }
+
+    public function action_create() {
+	echo 'Main controller action create'; //TODO delete 
+	//TODO action
+    }
+
 }
---
+++ app/views/page404.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Страница не найдена</title>
+    </head>
+    <body>
+	Страница не найдена
+	<img src="http://blog.ubiq.co/wp-content/uploads/2018/11/redirect-404-error-page-to-home-page.jpg" alt="not found"/>
+    </body>
+</html>
